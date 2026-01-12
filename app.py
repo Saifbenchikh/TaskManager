33f86859b8efd1285e1ccc444fa6f4663040d9da
BRYOCHE <benchikhislam1@gmail.com>
2026-01-12 22:07:55 +0100

    ajout date

--- app.py
+++ app.py
@@ -1,43 +1,39 @@
-from flask import Flask, render_template, request, redirect
 import sqlite3
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+# Fonction pour se connecter à la base de données
 def get_db_connection():
     conn = sqlite3.connect('database.db')
-    # Cette ligne permet d'accéder aux colonnes par leur nom (ex: tache['titre'])
-    conn.row_factory = sqlite3.Row 
+    conn.row_factory = sqlite3.Row  # Permet d'appeler les colonnes par leur nom
     return conn
 
 @app.route("/")
 def accueil():
     conn = get_db_connection()
-    # On récupère toutes les tâches de la base de données
+    # On récupère tout depuis la table 'tasks' (nom choisi par Martin)
     taches = conn.execute('SELECT * FROM tasks').fetchall()
     conn.close()
     return render_template("index.html", taches=taches)
 
-
 @app.route('/ajouter', methods=['POST'])
 def ajouter_tache():
-
+    # 1. Récupération des données du formulaire HTML
     titre_recu = request.form.get('titre')
-    
-    # On insère la nouvelle tâche dans la BDD
+    date_recue = request.form.get('date') # On récupère la date choisie
+
+    # 2. Enregistrement dans la Base de Données
     conn = get_db_connection()
-    conn.execute('INSERT INTO tasks (titre, statut, urgence) VALUES (?, ?, ?)',
-                 (titre_recu, 'A faire', 'primary'))
+    
+    # On insère le titre ET la date_echeance
+    conn.execute('INSERT INTO tasks (titre, statut, urgence, date_echeance) VALUES (?, ?, ?, ?)',
+                 (titre_recu, 'A faire', 'primary', date_recue))
+                 
     conn.commit()
     conn.close()
-  
 
     return redirect('/')
-
-
-
-
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
